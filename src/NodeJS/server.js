6214abb1c8c4b42096f4a82d0ce02edb37a16ac1
Alamay <khvechuksergiyko@gmail.com>
2023-04-06 21:23:44 +0300

    Added node.js

---
+++ src/NodeJS/server.js
@@ -0,0 +1,56 @@
+const http = require('http');
+const fs = require('fs');
+
+const server = http.createServer((request, response) => {
+    const { method, url } = request;
+
+    // Обробка запитів GET
+    if (method === 'GET') {
+        if (url === '/data') {
+            // Отримання даних з бази даних або з іншого джерела
+            const data = { message: 'Hello, world!' };
+
+            // Встановлення заголовків відповіді
+            response.setHeader('Content-Type', 'application/json');
+            response.setHeader('Access-Control-Allow-Origin', '*');
+
+            // Відправлення даних як відповідь на запит
+            response.write(JSON.stringify(data));
+            response.end();
+        } else if (url === '/enterprise') {
+            // Читання даних з JSON-файлу
+            fs.readFile("./DateJSON/Enterprises.json", (err, data) => {
+                if (err) {
+                    console.error(err);
+                    response.writeHead(500, { 'Content-Type': 'text/plain' });
+                    response.write('Internal server error');
+                    response.end();
+                    return;
+                }
+
+                // Встановлення заголовків відповіді
+                response.setHeader('Content-Type', 'application/json');
+                response.setHeader('Access-Control-Allow-Origin', '*');
+
+                // Відправлення даних як відповідь на запит
+                response.write(data);
+                response.end();
+            });
+        }else if (url === '/currency') {
+            // Отримання даних з бази даних або з іншого джерела
+            const currency = { name: 'UAH'};
+
+            // Встановлення заголовків відповіді
+            response.setHeader('Content-Type', 'application/json');
+            response.setHeader('Access-Control-Allow-Origin', '*');
+
+            // Відправлення даних як відповідь на запит
+            response.write(JSON.stringify(currency));
+            response.end();
+        }
+    }
+});
+
+server.listen(4000, () => {
+    console.log('Server is running on port 4000');
+});
