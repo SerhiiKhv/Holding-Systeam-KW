2c432a4ff9ec55f4e326e0f8dcacfc46a0b2f245
Alamay <khvechuksergiyko@gmail.com>
2023-06-03 19:00:45 +0300

    Update StartPage

--- src/Component/Content/StartPage/StartPage.tsx
+++ src/Component/Content/StartPage/StartPage.tsx
@@ -1,5 +1,33 @@
+import style from "./StartPage.module.css";
+import telegramLogo from "../../../Assets/img/telegram.svg.webp"
+import gitHubLogo from "../../../Assets/img/gitHub.png"
+
 export const StartPage = () => {
-    return <div>
-        StartPage
-    </div>
-}
+    return (
+        <div className={style.startPage}>
+            <div className={style.topPage}>StartPage</div>
+            <div className={style.underPage}>
+                <ul className={style.footerLinks}>
+                    <li>
+                        <a href="https://github.com/SerhiiKhv"
+                           target="_blank" rel="noopener noreferrer">
+                            <img
+                            src={gitHubLogo}
+                            alt={'GitHub'}
+                            />
+                        </a>
+                    </li>
+                    <li>
+                        <a href="https://xn--80affa3aj0al.xn--80asehdb/#@Alamay8"
+                           target="_blank" rel="noopener noreferrer">
+                            <img
+                                src={telegramLogo}
+                                alt={"Telegram"}
+                            />
+                        </a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    );
+};
